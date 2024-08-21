3fc3b98bf901dd9d1175f869355df41e82cf4697
manik-maity <manikmaity.haker2003@gmail.com>
2024-08-21 20:19:22 +0530

    Time: 67 ms (69.51%) | Memory: 50.8 MB (87.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,6 +4,41 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    /*
     // brute forse approch -> 
     return s.split("").sort().join() == t.split("").sort().join();
+    */
+    
+    // using frequiency map => 
+    if (s.length !== t.length) return false;
+
+    let map = {};
+    for(let i = 0; i < s.length; i++){
+        if (map[s[i]]){
+            map[s[i]] += 1;
+        }
+        else {
+            map[s[i]] = 1;
+        }
+    }
+
+    for(let j = 0; j < t.length; j++){
+        if (map[t[j]]){
+            map[t[j]] -= 1;
+            if (map[t[j]] == 0){
+                delete map[t[j]];
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    if(Object.keys(map).length == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+
 };
