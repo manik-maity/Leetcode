272efc700dd00b66d52853b6f4e13c2ef8f084e1
manik-maity <manikmaity.haker2003@gmail.com>
2024-08-27 00:00:14 +0530

    Time: 66 ms (85.80%) | Memory: 53.1 MB (35.99%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -6,6 +6,7 @@ var reverse = function(x) {
     // made a variable to store the reverse number
     let n = x;
 
+    // negetive -> positive
     if (n < 0){
         n = n*(-1);
     }
@@ -18,10 +19,12 @@ var reverse = function(x) {
         let lNum = n%10;
         // stronging last num in reverse
         rNum = (rNum * 10) + lNum;
+        // checking if exeded 32 bit num
         if (rNum > 2147483647) return 0;
         n = Math.floor(n/10); 
     }
 
+    // positive -> negetive
     if (x < 0){
         rNum = rNum*(-1);
     }
