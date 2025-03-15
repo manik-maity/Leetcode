b0c0d9b29e5b4fb63d82879286d51746695dd4bd
manik-maity <manikmaity.haker2003@gmail.com>
2025-03-15 20:57:24 +0530

    Time: 1 ms (95.20%) | Memory: 59.4 MB (40.93%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.ts
@@ -0,0 +1,17 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let maxCount = 0;
+  let currentCount = 0;
+  for (let i = 0; i < nums.length; i ++){
+    if (nums[i] == 1){
+      currentCount++;
+      if (currentCount > maxCount){
+        maxCount = currentCount;
+      }
+    }
+    else {
+      currentCount = 0;
+    }
+  }
+
+  return maxCount;
+};
