a4c5e4f173baffecc6273263ca1e6b4a5e96784b
manik-maity <manikmaity.haker2003@gmail.com>
2025-03-19 19:36:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.js
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => string.trim());
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine()); // Number of test cases
+    for (let i = 0; i < t; i++) {
+        let arr = readLine().split(' ').map(x => parseInt(x)); // Input array
+        let obj = new Solution();
+        const result = obj.leaders(arr);
+
+        // Print the result in the required format
+        if (result.length > 0) {
+            console.log(result.join(' ')); // Print leaders
+        } else {
+            console.log("[]"); // Print empty list if no leaders are found
+        }
+        console.log("~"); // Print empty list if no leaders are found
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {number[]} a
+ * @returns {number[]}
+ */
+class Solution {
+    // Function to find the leaders in the array.
+    leaders(a) {
+       let maxRight = a[a.length - 1];
+    const result  = [];
+    for (let i = a.length - 1; i >= 0; i--){
+        if (i === a.length - 1){
+            result.push(a[i]);
+            continue;
+        }
+        if (a[i] >= maxRight) {
+            result.push(a[i]);
+            maxRight = a[i];
+        }
+    }
+
+    return result.reverse();
+    }
+}
