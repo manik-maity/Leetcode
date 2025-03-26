8e23f3eb21da61320afc3e3e425cca681ce3560c
manik-maity <manikmaity.haker2003@gmail.com>
2025-03-26 11:14:42 +0530

    Time: 5 ms (54.44%) | Memory: 58 MB (53.33%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.ts
+++ 229-majority-element-ii/majority-element-ii.ts
@@ -1,21 +1,40 @@
 function majorityElement(nums: number[]): number[] {
-    const hashMap = new Map();
-    const numOfAppear = nums.length / 3;
-    const result : number[] = [];
+  let ele1, ele2, count1 = 0, count2 = 0;
+  for (let i = 0; i < nums.length; i++){
+    if (count1 == 0 && nums[i] !== ele2){
+        ele1 = nums[i];
+        count1 = 1;
+    }
+    else if(count2 == 0 && nums[i] !== ele1){
+        ele2 = nums[i];
+        count2 = 1;
+    }
+    else if (nums[i] == ele1){
+        count1++;
+    }
+    else if (nums[i] == ele2){
+        count2++;
+    }
+    else {
+        count1--;
+        count2--;
+    }
+  }
+    let rCount1 = 0, rCount2 = 0;
+    const result = [];
     for (let i = 0; i < nums.length; i++){
-        if (hashMap.has(nums[i])){
-            hashMap.set(nums[i], hashMap.get(nums[i]) + 1)
-        }
-        else {
-            hashMap.set(nums[i], 1);
+        if (nums[i] == ele1){
+            rCount1++;
         }
     }
 
-    hashMap.forEach((val, key) => {
-        if (val > numOfAppear){
-            result.push(key);
+     for (let i = 0; i < nums.length; i++){
+        if (nums[i] == ele2){
+            rCount2++;
         }
-    });
+    }
+    if (rCount1 > (nums.length / 3)) result.push(ele1);
+    if (rCount2 > (nums.length / 3)) result.push(ele2);
 
     return result;
 };
